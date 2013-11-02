a9be7edff17c3542172b280c1d91767cce066f0a
Matthew Shelley <matt@visualmarvel.com>
2013-11-02 02:45:57 -0700

    getting grunt setup, but hiding the modules from being committed into the repo, just the grunt configs

---
+++ GruntFile.js
@@ -0,0 +1,79 @@
+module.exports = function(grunt) {
+
+	/**
+	 * Project configuration.
+	 */
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		uglify: {
+			options: {
+				banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+			},
+			// build: {
+			// 	src: 'js/<%= pkg.name %>.js',
+			// 	dest: 'js/<%= pkg.name %>.min.js'
+			// }
+		},
+
+		/**
+		 * Watch our files for updates
+		 */
+		watch: {
+			gruntfile: {
+				files: 'Gruntfile.js',
+				tasks: ['jshint:gruntfile'],
+			},
+
+			options: {
+				livereload: true,
+			},
+
+			scripts: {
+				files: ['js/*.js'],
+				tasks: ['jshint'],
+				// options: {
+				// 	nospawn: true,
+				// },
+			},
+		},
+
+		/**
+		 * Run this using :: grunt jshint
+		 */
+		jshint: {
+			// beforeconcat: ['js/image-reveal.js'],
+			// afterconcat: ['js/image-reveal.js'],
+
+			files: ['js/*.js'],
+			options: {
+				ignores: ['js/eventShim.js', 'js/modernizr.js', 'js/scripts.min.js'],
+				// curly: true,
+				// eqeqeq: true,
+				// eqnull: true,
+				// browser: true,
+				// globals: {
+				// jQuery: true
+				// },
+			}
+		}
+
+	});
+
+	/**
+	 * Load our plugins
+	 */
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+
+	/**
+	 * Default task(s).
+	 */
+	grunt.registerTask('default', ['uglify']);
+
+	/**
+	 * On watch events configure jshint:all to only run on changed file
+	 */
+	grunt.event.on('watch', function(action, filepath) {
+		grunt.config('jshint.all.src', filepath);
+	});
+};
